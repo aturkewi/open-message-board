5f1ac6250403f9911cf694cc9da24304fed74455
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-11-26 11:34:02 -0500

    Message boards load on mount

--- src/components/MessageBoards.js
+++ src/components/MessageBoards.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
-const MessageBoards = props =>(
-  <div>
-    These are a list of all of my messages boards
-  </div>
-);
+class MessageBoards extends React.Component{
+  constructor(){
+    super();
+    
+    this.state={
+      messageBoards: []
+    }
+  }
+  componentWillMount(){
+    fetch('https://open-message-board-api.herokuapp.com/message_boards')
+      .then(res => res.json())
+      .then(json => this.setState({messageBoards: json}));
+  }
+  render(){
+    return(
+      <div>
+        <h1>Message Boards</h1>
+        <ul>
+          {this.state.messageBoards.map((mB => (
+            <li key={mB.id}>{mB.name}</li>
+          )))}
+        </ul>
+      </div>
+    )
+  }
+};
 
 module.exports = MessageBoards;
