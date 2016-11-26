233c203f3916dec449d7cae94a7c448e297cd34b
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-11-26 13:12:00 -0500

    Loading data in Message boards, working on Post request

--- src/components/MessageBoards.js
+++ src/components/MessageBoards.js
@@ -1,23 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router'
 
+// import Messages from './Messages';
+import NewBoard from './NewBoard'
+
 class MessageBoards extends React.Component{
   constructor(){
     super();
+    
+    this.createNewBoard = this.createNewBoard.bind(this);
     
     this.state={
       messageBoards: []
     }
   }
+  
   componentWillMount(){
     fetch('https://open-message-board-api.herokuapp.com/message_boards')
       .then(res => res.json())
       .then(json => this.setState({messageBoards: json}));
   }
+  
+  createNewBoard(ev){
+    ev.preventDefault()
+    const newName = ev.target.elements[0].value;
+    const data = new FormData();
+    const payLoad = {name: newName}
+    data.append( "message_board", JSON.stringify( payLoad ) );
+
+    fetch('http://localhost:3001/message_boards',{
+      headers: new Headers({
+		    'Content-Type': 'test/json'
+      }),
+      method: 'post',
+      body: data
+    })
+    .then(res => res.json())
+    .then(json => console.log(json))
+  }
+  
   render(){
     return(
       <div>
         <h1>Message Boards</h1>
+        <NewBoard onSubmit={this.createNewBoard}/>
         <ul>
           {this.state.messageBoards.map((mB => (
             <li key={mB.id}>
